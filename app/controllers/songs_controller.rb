d8f94e0e1f43fca1bef79fd3a613bda9e4990aa8
SiamKing <thesiameseelectric@gmail.com>
2016-06-30 20:34:08 -0700

    mothers little helper method

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,46 +25,68 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
-    @song = Song.new(song_params)
-
-    if @song.save
-      redirect_to @song
+    if params[:song][:artist_name] != ""
+      @song = Song.new(song_params(:title, :artist_name))
+      if @song.save
+        redirect_to @song
+      else
+        render :new
+      end
     else
-      render :new
+      @song = Song.new(song_params(:title, :artist_id))
+      if @song.save
+        redirect_to @song
+      else
+        render :new
+      end
     end
   end
 
-  def edit
-    @song = Song.find(params[:id])
-  end
+    def edit
+      if params[:artist_id]
+        @nested = true
+        artist = Artist.find_by(id: params[:artist_id])
+        if artist.nil?
+          redirect_to artists_path, alert: "Artist not found."
+        else
+          @song = artist.songs.find_by(id: params[:id])
+          redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
+        end
+      else
+        @song = Song.find(params[:id])
+      end
+    end
 
-  def update
-    @song = Song.find(params[:id])
+    def update
+      @song = Song.find(params[:id])
 
-    @song.update(song_params)
+      @song.update(song_params(:title, :artist_id, :artist_name))
 
-    if @song.save
-      redirect_to @song
-    else
-      render :edit
+      if @song.save
+        redirect_to @song
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @song = Song.find(params[:id])
+      @song.destroy
+      flash[:notice] = "Song deleted."
+      redirect_to songs_path
+    end
+
+    private
+
+    def song_params(*args)
+      params.require(:song).permit(*args)
     end
   end
-
-  def destroy
-    @song = Song.find(params[:id])
-    @song.destroy
-    flash[:notice] = "Song deleted."
-    redirect_to songs_path
-  end
-
-  private
-
-  def song_params
-    params.require(:song).permit(:title, :artist_name)
-  end
-end
-
